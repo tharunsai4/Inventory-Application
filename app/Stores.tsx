68c3fb57385f9c1b019d5fd012d3d94d3f99204e
Tharun sai <tharunsai@Tharuns-MacBook-Air.local>
2025-07-13 16:23:56 +0100

    added routes

---
+++ app/Stores.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Stores = () => {
+  return (
+    <View>
+      <Text>Stores</Text>
+    </View>
+  )
+}
+
+export default Stores
